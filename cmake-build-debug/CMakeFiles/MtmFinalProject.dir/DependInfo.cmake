4478b800377be06ebeb3a33a1948b9082fc211c4
Mohammad Mossa <mohammed.19.03@hotmail.com>
2020-08-07 13:11:00 +0300

    Cmake shit

---
+++ cmake-build-debug/CMakeFiles/MtmFinalProject.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/Documents/GitHub/WizardCell/MtmFinalProject/Edge.cpp" "F:/Documents/GitHub/WizardCell/MtmFinalProject/cmake-build-debug/CMakeFiles/MtmFinalProject.dir/Edge.cpp.obj"
+  "F:/Documents/GitHub/WizardCell/MtmFinalProject/Graph.cpp" "F:/Documents/GitHub/WizardCell/MtmFinalProject/cmake-build-debug/CMakeFiles/MtmFinalProject.dir/Graph.cpp.obj"
+  "F:/Documents/GitHub/WizardCell/MtmFinalProject/Vertex.cpp" "F:/Documents/GitHub/WizardCell/MtmFinalProject/cmake-build-debug/CMakeFiles/MtmFinalProject.dir/Vertex.cpp.obj"
+  "F:/Documents/GitHub/WizardCell/MtmFinalProject/main.cpp" "F:/Documents/GitHub/WizardCell/MtmFinalProject/cmake-build-debug/CMakeFiles/MtmFinalProject.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
